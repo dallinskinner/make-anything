57e5ecc64629c5b1fff8fb00250d580e8ba148cb
Dallin Skinner <dallinskinner@gmail.com>
2025-05-08 17:40:59 -0600

    Fix script

--- src/components/FeedbackToast.tsx
+++ src/components/FeedbackToast.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useRef, useState } from "react";
 import { Alert, Button, Modal, Toast } from "react-daisyui";
+import Script from "next/script";
 
 export function FeedbackToast() {
   const ref = useRef<HTMLDialogElement>(null);
@@ -22,7 +23,7 @@ export function FeedbackToast() {
 
       <Modal ref={ref}>
         <div data-tf-live="01JTM7KTYCZD7F8KA24FM3CXF6"></div>
-        <script src="//embed.typeform.com/next/embed.js"></script>
+        <Script src="//embed.typeform.com/next/embed.js"></Script>
         <p className="text-xs mt-2">Press ESC to close</p>
       </Modal>
     </>
